de4d800cfe45af423a71f73cf56837d68fc4bc18
pilip-d <philipdell@gmx.de>
2024-05-20 14:14:40 +0200

    expose converter classes

--- src/file_crusher/__init__.py
+++ src/file_crusher/__init__.py
@@ -5,7 +5,11 @@ from .png_compressor import PNGCompressor
 from .pngquant_compressor import PNGQuantCompressor
 from .pngcrush_compressor import PNGCrushCompressor
 from .pipeline_processor import PipelineProcessor
+from .converter.image_to_pdf_converter import ImagesToPdfConverter
+from .converter.pdf_to_image_converter import PdfToImageConverter
+from .converter.pdf_merger import merge_pdf_files
 from .batch_processor import batch_process_files_async, batch_process_files
 
 __all__ = ["CPdfSqueezeCompressor", "ADVPNGCompressor", "PDFCompressor", "PNGCompressor", "PNGQuantCompressor",
-           "PNGCrushCompressor", "PipelineProcessor", "batch_process_files", "batch_process_files_async"]
+           "PNGCrushCompressor", "PipelineProcessor", "PdfToImageConverter", "ImagesToPdfConverter",
+           "batch_process_files", "batch_process_files_async", "merge_pdf_files"]
